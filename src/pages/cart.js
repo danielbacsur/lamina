f40f6f6b0bd30fcfc7da071f5f5815ddcc82ac44
danielbacsur <contact@danielbacsur.com>
2023-01-06 23:27:22 +0100

    oksa

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,31 +1,27 @@
-import { Router, useRouter } from "next/router";
 import { useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 const Cart = () => {
   const router = useRouter();
-  const { status } = router.query;
-
   const [items, setItems] = useState([
     {
       name: "Starboy",
       description: "The Weekend",
-      image:
-        "https://m.media-amazon.com/images/I/819e05qxPEL.jpg",
+      image: "https://m.media-amazon.com/images/I/819e05qxPEL.jpg",
       quantity: 1,
       price: 5000,
     },
     {
       name: "Redbone",
       description: "Childish Gambino",
-      image:
-        "https://m.media-amazon.com/images/I/81aYuDTpo3L.jpg",
+      image: "https://m.media-amazon.com/images/I/81aYuDTpo3L.jpg",
       quantity: 2,
       price: 4750,
     },
   ]);
 
-  const submit = async (e) => {
+  const checkout = async (e) => {
     e.preventDefault();
     const { data } = await axios.post("/api/checkout", {
       items: items,
@@ -35,9 +31,10 @@ const Cart = () => {
 
   return (
     <>
-      <form onSubmit={submit}>
+      <form onSubmit={checkout}>
         <button type="submit">Checkout</button>
       </form>
+      <br />
     </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 export default function Home() {
+  const example = {
+    title: "Starboy",
+    artist: "The Weekend",
+    image: "https://m.media-amazon.com/images/I/819e05qxPEL.jpg",
+    quantity: 4,
+    price: 5000,
+    subval: 500,
+  };
+
+  console.log((example.price * example.quantity - (example.quantity-1) * 2000) / example.quantity);
+
   return <div>alma</div>;
 }
